918609c1a6a534dd95a103e250830a367a36347b
송치윤 <songcchiyoon@gmail.com>
2017-07-20 19:40:51 +0900

    convert list to np

--- Model.py
+++ Model.py
@@ -6,10 +6,26 @@ import dataparse
 
 from gensim.models.keyedvectors import KeyedVectors
 wordModel = KeyedVectors.load_word2vec_format('model/w2v_model', binary=False)
-featureModel = KeyedVectors.load_word2vec_format('model/featureEmbedding', binary=False)
+featureModel = KeyedVectors.load_word2vec_format('model/f2v_model', binary=False)
 
 import dataparse as ps
 
+data_path = 'data/train.txt'
+parser = ps.Parser(data_path)
+parser.parse()
+sentences = parser.sentencs
 
 
+labels = parser.labels
 
+x_data = np.array(sentences)
+y_data = np.array(labels)
+
+print(y_data.shape)
+print(x_data.shape)
+
+X = tf.placeholder(tf.int32, [None, None])  # X data
+Y = tf.placeholder(tf.int32, [None, None])  # Y label
+
+
+#y_one_hot = tf.one_hot(Y, num_classes)  # one hot: 1 -> 0 1 0 0 0 0 0 0 0 0
