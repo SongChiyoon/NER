b329b84f0351ef20d4d45c8c81cd81403ca0ba23
송치윤 <songcchiyoon@gmail.com>
2017-07-27 15:07:38 +0900

    Batch train building

--- Model.py
+++ Model.py
@@ -8,6 +8,8 @@ from gensim.models.keyedvectors import KeyedVectors
 wordModel = KeyedVectors.load('model/w2v_model')
 featureModel = KeyedVectors.load('model/f2v_model')
 
+
+
 import dataparse as ps
 #train data parsing
 data_path = 'data/train.txt'
@@ -23,12 +25,13 @@ labels = parser.labels
 input_data = []
 entity_data = []
 sequence_length = []
+batch_size = 20
 n_class = len(category)  #number of labels
 
 max_len = 336
 vec_size = 50
 learning_rate = 0.01
-#empty = 'empty'
+empty = 'empty'
 label_dic = {
     'O' : [1,0,0,0,0,0,0],
     'B_OG' : [0,1,0,0,0,0,0],
@@ -43,22 +46,23 @@ label_dic = {
 
 for line in sentences:
     w2c = []
-    #if len(line) < max_len:
-        #for i in range(max_len - len(line)):
-            #line.append(empty)
+    sequence_length.append(len(line))
+    if len(line) < max_len:
+        for i in range(max_len - len(line)):
+            line.append(empty)
     for i in line:
-        #if empty == i:
-            #w2c.append(np.zeros(vec_size))
-        #else:
-        w2c.append(wordModel[i])
+        if empty == i:
+            w2c.append(np.zeros(vec_size))
+        else:
+            w2c.append(wordModel[i])
     input_data.append(w2c)
 
 # remove zero padding
 for line in labels:
     l = []
-    #if len(line) < max_len:
-        #for i in range(max_len-len(line)):
-            #line.append(empty)
+    if len(line) < max_len:
+        for i in range(max_len-len(line)):
+            line.append(empty)
     for i in line:
         l.append(label_dic[i])
     entity_data.append(l)
@@ -75,17 +79,17 @@ category = np.array(category)
 # ['O' 'B_OG' 'I' 'B_DT' 'B_PS' 'B_LC' 'B_TI']
 
 
-X = tf.placeholder(tf.float32, [None, max_len, 50])  # X data
-Y = tf.placeholder(tf.float32, [None, max_len, n_class])  # Y label
+X = tf.placeholder(tf.float32, [None, 336, 50])  # X data
+Y = tf.placeholder(tf.float32, [None, 336, n_class])  # Y label
+seq_len = tf.placeholder(tf.int32, [None])
 
-
-
+print(sequence_length[3:5])
 
 cell = tf.contrib.rnn.BasicLSTMCell(max_len, state_is_tuple=True)
 #cell = tf.contrib.rnn.MultiRNNCell([cell] * 2, state_is_tuple=True)
 initial_state = cell.zero_state(1, tf.float32)
 
-outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32, sequence_length=seq_len)
 
 Y_pred = tf.contrib.layers.fully_connected(outputs, n_class, activation_fn=None)
 
@@ -95,6 +99,8 @@ optm = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 corr = tf.equal(tf.argmax(Y_pred, 2), tf.argmax(Y, 2))
 accr = tf.reduce_mean(tf.cast(corr, tf.float32))
 
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
 
 #Saver
 save_dir = "model_2/"
@@ -107,19 +113,35 @@ print ("FUNCTIONS READY")
 
 iteration = 50
 save_step = 2
-batch_size = 20
 n_train = x_data.shape[0]
 show_step = 2
 pretrained = True
 # LAUNCH THE GRAPH
 
-x = x_data[None, :]
-print(x.shape)
 sess = tf.Session()
 sess.run(init)
+sequence_length = np.array(sequence_length)
+print(sess.run(cost, feed_dict={X:x_data[:1], Y:y_data[:1], seq_len:sequence_length[:1]}))
+
+for iter in range(iteration):
+    total_batch = int(n_train / batch_size)
+    randpermlist = np.random.permutation(n_train)
+    sum_cost = 0
+    for i in range(total_batch):
+        randidx = randpermlist[i * batch_size:min((i + 1) * batch_size, n_train - 1)]
+        print(randidx)
+        if i == 5:
+            print(sequence_length[randidx])
+            break
+    break
 '''
 print ("Training start")
 for iter in range(iteration):
+    sess.run(optm, feed_dict={X:x_data, Y:y_data})
+    if iter % 5 == 0:
+        print(sess.run(cost, feed_dict={X:x_data, Y:y_data}))'''
+
+'''for iter in range(iteration):
     total_batch = int(n_train / batch_size)
     randpermlist = np.random.permutation(n_train)
     sum_cost = 0
