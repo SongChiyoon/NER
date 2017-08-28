1a32fa79e36d01bf6fb89ca5e3f9f31ae965402e
송치윤 <songcchiyoon@gmail.com>
2017-08-28 21:00:53 +0900

    Data convert

---
+++ build_dataformat.py
@@ -0,0 +1,122 @@
+class textObject(object):
+    def __init__(self, path):
+        self.positions = []
+        self.words = []
+        self.features = []
+        self.labels = []
+
+    def setLine(self, line):
+        self.line = line
+
+    def getLine(self):
+        return self.line
+
+
+    def setResult(self, l):
+        self.result = l
+
+    def getResult(self):
+        return self.result
+
+    def addSplits(self, position, word, feature, label):
+        self.positions.append(position)
+        self.words.append(word)
+        self.features.append(feature)
+        self.labels.append(label)
+        #print(self.words)
+
+    def getWord(self):
+        return self.words
+
+class Parser(object):
+    def __init__(self, path, store_path):
+        self.path = path
+        self.store_path = store_path
+        self.text2Object = []
+        self.sentences = []
+        self.features = []
+        self.labels = []
+        self.catagory = []
+
+    def sentences(self):
+        return self.sentences
+
+    def features(self):
+        return self.features
+
+    def labels(self):
+        return self.labels
+
+    def category(self):
+        return self.catagory
+
+    def parse(self):
+        text = None
+        appending = False
+        words = []
+        fe = []
+        label = []
+        max_len = -1
+        min_len = 10000
+        fw = open(self.store_path, 'w')
+        with open(self.path, 'r') as f:
+            for line in f:
+                if ";" in line and "." in line:
+                    if not appending:
+                        print("start")
+                        appending = True
+
+                    if appending and text is not None:
+                        fw.write("\n")
+                        self.text2Object.append(text)
+                        self.sentences.append(words)
+                        self.features.append(fe)
+                        self.labels.append(label)
+                        if len(words) > max_len:
+                            max_len = len(words)
+                        if len(words) < min_len and len(words) != 0:
+                            min_len = len(words)
+                        label = []
+                        fe = []
+                        words = []
+                    text = textObject(object)
+                    text.setLine(line[2:])
+                    continue
+                if "$" in line and "." in line:
+                    text.setResult(line[1:])
+                    continue
+                if appending:
+                    line = line.replace("\n", "")
+                    if "" == line:
+                        continue
+                    splits = line.split("\t")
+                    # print(len(splits))
+                    if len(splits) == 4:
+                        text.addSplits(splits[0], splits[1], splits[2], splits[3])
+                        words.append(splits[1]+"/"+splits[2])
+                        fw.write(splits[1]+"\t"+splits[2]+"\t"+splits[3]+"\n")
+                        fe.append(splits[2])
+                        label.append(splits[3])
+                        if splits[3] not in self.catagory:
+                            self.catagory.append(splits[3])
+                    continue
+            fw.close()
+            print('max len :{0}'.format(max_len))
+            print('min len :{0}'.format(min_len))
+
+datapath = "data/test.txt"
+store_path = "data/test_format.txt"
+parser = Parser(datapath, store_path)
+parser.parse()
+
+
+datapath = "data/train.txt"
+store_path = "data/train_format.txt"
+parser = Parser(datapath, store_path)
+parser.parse()
+
+
+datapath = "data/dev.txt"
+store_path = "data/dev_format.txt"
+parser = Parser(datapath, store_path)
+parser.parse()
